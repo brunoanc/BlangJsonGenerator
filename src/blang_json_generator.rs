b33bd035d59f716e4174ff80603c018677115e88
PowerBall253 <brunoanconasala@gmail.com>
2021-03-30 21:03:16 -0600

    Various improvements in the code

--- src/blang_json_generator.rs
+++ src/blang_json_generator.rs
@@ -60,25 +60,32 @@ struct BlangJson {
     strings: Vec<BlangJsonString>
 }
 
-fn parse_blang(blang_bytes: Vec<u8>) -> Vec<BlangString> {
+fn parse_blang(blang_bytes: Vec<u8>, is_new_format: bool) -> Vec<BlangString> {
     let mut blang_strings: Vec<BlangString> = Vec::new();
 
     let string_amount = i32::from_be_bytes(blang_bytes[8..12].try_into().unwrap());
 
-    let mut pos = 12;
+    let mut pos: usize;
+    if is_new_format {
+        pos = 12;
+    }
+    else {
+        pos = 4;
+    }
+
     for _i in 0..string_amount {
         pos += 4;
 
         let identifier_len = i32::from_le_bytes(blang_bytes[pos..(pos + 4)].try_into().unwrap()) as usize;
         pos += 4;
 
-        let identifier = str::from_utf8(&blang_bytes[pos..(pos + identifier_len)]).unwrap().to_string();
+        let identifier = String::from_utf8(blang_bytes[pos..(pos + identifier_len)].to_vec()).unwrap().to_string();
         pos += identifier_len;
 
         let text_len = i32::from_le_bytes(blang_bytes[pos..(pos + 4)].try_into().unwrap()) as usize;
         pos += 4;
 
-        let text = str::from_utf8(&blang_bytes[pos..(pos + text_len)]).unwrap().to_string();
+        let text = String::from_utf8(blang_bytes[pos..(pos + text_len)].to_vec()).unwrap();
         pos += text_len;
 
         let unknown_len = i32::from_le_bytes(blang_bytes[pos..(pos + 4)].try_into().unwrap()) as usize;
@@ -137,7 +144,7 @@ fn id_crypt(file_data: Vec<u8>, internal_path: String) -> Vec<u8> {
 fn generate_blang_json(path: String, internal_path: String, out_path: String) {
     if !Path::new(&path).exists() {
         eprintln!("ERROR: {} does not exist!", path);
-        exit(101);
+        return;
     }
 
     let mut file = File::open(&path).expect("Failed to open file for reading.");
@@ -146,15 +153,27 @@ fn generate_blang_json(path: String, internal_path: String, out_path: String) {
 
     let blang_bytes = id_crypt(file_data.clone(), internal_path.to_string());
 
-    let result = panic::catch_unwind(|| {
-        parse_blang(blang_bytes.clone());
+    panic::set_hook(Box::new(|_info| {}));
+
+    let mut is_new_format = true;
+
+    let mut result = panic::catch_unwind(|| {
+        parse_blang(blang_bytes.clone(), is_new_format);
     });
     if result.is_err() {
-        eprintln!("ERROR: Failed to parse {}.", path);
-        exit(101);
+        is_new_format = false;
+        result = panic::catch_unwind(|| {
+            parse_blang(blang_bytes.clone(), is_new_format);
+        });
+        if result.is_err() {
+            eprintln!("ERROR: Failed to parse {}.", path);
+            return;
+        }
     }
 
-    let blang_strings = parse_blang(blang_bytes);
+    let _ = panic::take_hook();
+
+    let blang_strings = parse_blang(blang_bytes, is_new_format);
 
     let mut strings: Vec<FullBlangJsonString> = Vec::new();
 
@@ -173,16 +192,21 @@ fn generate_blang_json(path: String, internal_path: String, out_path: String) {
     };
 
     let j = serde_jsonrc::to_string_pretty(&blang_json).unwrap_or_else(|_| {
-        eprintln!("ERROR: Failed to parse {}.", path);
-        exit(101);
+        return "".to_string();
     });
+
+    if j.is_empty() {
+        eprintln!("ERROR: Failed to parse {}.", path);
+        return;
+    }
 
     if Path::new(&out_path).exists() {
         print!("{} already exists! Overwrite it? [y/N] ", out_path);
         io::stdout().flush().unwrap();
         let response: String = read!();
+        println!("");
         if response != "y" && response != "Y" {
-            exit(101);
+            return;
         }
     }
 
@@ -195,7 +219,7 @@ fn generate_blang_json(path: String, internal_path: String, out_path: String) {
 fn generate_strings_json(path: String, out_path: String) {
     if !Path::new(&path).exists() {
         eprintln!("ERROR: {} does not exist!", path);
-        exit(101);
+        return;
     }
 
     let mut file = File::open(&path).expect("Failed to open file for reading.");
@@ -203,9 +227,16 @@ fn generate_strings_json(path: String, out_path: String) {
     file.read_to_string(&mut blang_json_string).expect("Failed to read file.");
 
     let blang_json: FullBlangJson = serde_jsonrc::from_str(&blang_json_string).unwrap_or_else(|_| {
-        eprintln!("ERROR: Failed to parse {}.", path);
-        exit(101);
+        let empty_blang_json = FullBlangJson {
+            strings: Vec::new(),
+        };
+        return empty_blang_json;
     });
+
+    if blang_json.strings.is_empty() {
+        eprintln!("ERROR: Failed to parse {}.", path);
+        return;
+    }
 
     let mut modified_strings: Vec<BlangJsonString> = Vec::new();
 
@@ -224,16 +255,21 @@ fn generate_strings_json(path: String, out_path: String) {
     };
 
     let j = serde_jsonrc::to_string_pretty(&final_json).unwrap_or_else(|_| {
-        eprintln!("ERROR: Failed to parse {}.", path);
-        exit(101);
+        return "".to_string();
     });
+
+    if j.is_empty() {
+        eprintln!("ERROR: Failed to parse {}.", path);
+        return;
+    }
 
     if Path::new(&out_path).exists() {
         print!("{} already exists! Overwrite it? [y/N] ", out_path);
         io::stdout().flush().unwrap();
         let response: String = read!();
+        println!("");
         if response != "y" && response != "Y" {
-            exit(101);
+            return;
         }
     }
 
@@ -246,7 +282,7 @@ fn generate_strings_json(path: String, out_path: String) {
 fn add_new_string(path: String, identifier: String) {
     if !Path::new(&path).exists() {
         eprintln!("ERROR: {} does not exist!", path);
-        exit(101);
+        return;
     }
 
     let mut file = File::open(&path).expect("Failed to open file for reading.");
@@ -254,14 +290,21 @@ fn add_new_string(path: String, identifier: String) {
     file.read_to_string(&mut blang_json_string).expect("Failed to read file.");
 
     let mut blang_json: BlangJson = serde_jsonrc::from_str(&blang_json_string).unwrap_or_else(|_| {
-        eprintln!("ERROR: Failed to parse {}.", path);
-        exit(101);
+        let empty_blang_json = BlangJson {
+            strings: Vec::new(),
+        };
+        return empty_blang_json;
     });
+
+    if blang_json.strings.is_empty() {
+        eprintln!("ERROR: Failed to parse {}.", path);
+        return;
+    }
 
     for string in blang_json.strings.iter() {
         if string.name == identifier {
             eprintln!("ERROR: String already exists in the JSON file!");
-            exit(101);
+            return;
         }
     }
 
@@ -273,9 +316,13 @@ fn add_new_string(path: String, identifier: String) {
     blang_json.strings.push(new_string);
 
     let j = serde_jsonrc::to_string_pretty(&blang_json).unwrap_or_else(|_| {
-        eprintln!("ERROR: Failed to parse {}.", path);
-        exit(101);
+        return "".to_string();
     });
+
+    if j.is_empty() {
+        eprintln!("ERROR: Failed to parse {}.", path);
+        return;
+    }
 
     let mut out_file = File::create(&path).expect("Failed to open file for writing.");
     out_file.write_all(j.as_bytes()).expect("Failed to write new string to file.");
@@ -287,12 +334,12 @@ fn add_new_string(path: String, identifier: String) {
 fn modified_blang_to_json(modified_path: String, vanilla_path: String, internal_path: String, out_path: String) {
     if !Path::new(&modified_path).exists() {
         eprintln!("ERROR: {} does not exist!", modified_path);
-        exit(101);
+        return;
     }
 
     if !Path::new(&vanilla_path).exists() {
         eprintln!("ERROR: {} does not exist!", vanilla_path);
-        exit(101);
+        return;
     }
 
     let mut vanilla_file = File::open(&vanilla_path).expect("Failed to open file for reading.");
@@ -301,15 +348,27 @@ fn modified_blang_to_json(modified_path: String, vanilla_path: String, internal_
 
     let vanilla_blang_bytes = id_crypt(vanilla_file_data.clone(), internal_path.to_string());
 
+    panic::set_hook(Box::new(|_info| {}));
+
+    let mut is_new_format = true;
+
     let mut result = panic::catch_unwind(|| {
-        parse_blang(vanilla_blang_bytes.clone());
+        parse_blang(vanilla_blang_bytes.clone(), is_new_format);
     });
     if result.is_err() {
-        eprintln!("ERROR: Failed to parse {}.", vanilla_path);
-        exit(101);
+        is_new_format = false;
+        result = panic::catch_unwind(|| {
+            parse_blang(vanilla_blang_bytes.clone(), is_new_format);
+        });
+        if result.is_err() {
+            eprintln!("ERROR: Failed to parse {}.", vanilla_path);
+            return;
+        }
     }
 
-    let vanilla_strings = parse_blang(vanilla_blang_bytes);
+    let _ = panic::take_hook();
+
+    let vanilla_strings = parse_blang(vanilla_blang_bytes, is_new_format);
 
     let mut modified_file = File::open(&modified_path).expect("Failed to open file for reading.");
     let mut modified_file_data: Vec<u8> = Vec::new();
@@ -317,16 +376,27 @@ fn modified_blang_to_json(modified_path: String, vanilla_path: String, internal_
 
     let modified_blang_bytes = id_crypt(modified_file_data.clone(), internal_path.to_string());
 
-    result = panic::catch_unwind(|| {
-        parse_blang(modified_blang_bytes.clone());
-    });
+    panic::set_hook(Box::new(|_info| {}));
 
+    is_new_format = true;
+
+    result = panic::catch_unwind(|| {
+        parse_blang(modified_blang_bytes.clone(), is_new_format);
+    });
     if result.is_err() {
-        eprintln!("ERROR: Failed to parse {}.", modified_path);
-        exit(101);
+        is_new_format = false;
+        result = panic::catch_unwind(|| {
+            parse_blang(modified_blang_bytes.clone(), is_new_format);
+        });
+        if result.is_err() {
+            eprintln!("ERROR: Failed to parse {}.", modified_path);
+            return;
+        }
     }
 
-    let modified_strings = parse_blang(modified_blang_bytes);
+    let _ = panic::take_hook();
+
+    let modified_strings = parse_blang(modified_blang_bytes, is_new_format);
 
     let mut new_modified_strings: Vec<BlangJsonString> = Vec::new();
 
@@ -356,7 +426,7 @@ fn modified_blang_to_json(modified_path: String, vanilla_path: String, internal_
 
     if new_modified_strings.len() == 0 {
         eprintln!("ERROR: Failed to generate JSON - all strings are equal!");
-        exit(101);
+        return;
     }
 
     let final_json = BlangJson {
@@ -364,16 +434,21 @@ fn modified_blang_to_json(modified_path: String, vanilla_path: String, internal_
     };
 
     let j = serde_jsonrc::to_string_pretty(&final_json).unwrap_or_else(|_| {
-        eprintln!("ERROR: Failed to parse {}.", modified_path);
-        exit(101);
+        return "".to_string();
     });
+
+    if j.is_empty() {
+        eprintln!("ERROR: Failed to parse {}.", modified_path);
+        return;
+    }
 
     if Path::new(&out_path).exists() {
         print!("{} already exists! Overwrite it? [y/N] ", out_path);
         io::stdout().flush().unwrap();
         let response: String = read!();
+        println!("");
         if response != "y" && response != "Y" {
-            exit(101);
+            return;
         }
     }
 
@@ -384,98 +459,103 @@ fn modified_blang_to_json(modified_path: String, vanilla_path: String, internal_
 }
 
 fn main() {
-    println!("\nBlangJsonGenerator v1.0 by PowerBall253\n");
-    println!("1. Generate a full blang JSON.");
-    println!("2. Generate a string mod JSON using an edited full blang JSON.");
-    println!("3. Add a new string to a mod JSON.");
-    println!("4. Generate a string mod JSON from a modified blang file.");
-    println!("5. Exit.");
-
-    let mut response: String;
     loop {
-
-        print!("\nSelect an option: ");
-        io::stdout().flush().unwrap();
-        response = read!();
-        if response.parse::<u32>().is_ok() && response.parse::<i32>().unwrap() <= 5 {
-            break;
+        println!("\nBlangJsonGenerator v1.0 by PowerBall253\n");
+        println!("1. Generate a full blang JSON.");
+        println!("2. Generate a string mod JSON using an edited full blang JSON.");
+        println!("3. Add a new string to a mod JSON.");
+        println!("4. Generate a string mod JSON from a modified blang file.");
+        println!("5. Exit.");
+    
+        let mut response: String;
+        loop {
+            print!("\nSelect an option: ");
+            io::stdout().flush().unwrap();
+            response = read!();
+            if response.parse::<u32>().is_ok() && response.parse::<i32>().unwrap() <= 5 {
+                break;
+            }
+            println!("Invalid option!");
         }
-        println!("Invalid option!");
-    }
 
-    match response.parse::<i32>().unwrap() {
-        1 => {
-            print!("Input the path to the .blang file: ");
-            io::stdout().flush().unwrap();
-            let path: String = read!();
-
-            print!("Input the language name: ");
-            io::stdout().flush().unwrap();
-            let language: String = read!();
-            let mut internal_path = "strings/".to_string();
-            internal_path.push_str(language.as_str());
-            internal_path.push_str(".blang");
-
-            print!("Input the path to the output file: ");
-            io::stdout().flush().unwrap();
-            let out_path: String = read!();
-
-            println!("");
-
-            generate_blang_json(path, internal_path, out_path);
+        match response.parse::<i32>().unwrap() {
+            1 => {
+                print!("Input the path to the .blang file: ");
+                io::stdout().flush().unwrap();
+                let path: String = read!();
+    
+                print!("Input the language name: ");
+                io::stdout().flush().unwrap();
+                let language: String = read!();
+                let mut internal_path = "strings/".to_string();
+                internal_path.push_str(language.as_str());
+                internal_path.push_str(".blang");
+    
+                print!("Input the path to the output file: ");
+                io::stdout().flush().unwrap();
+                let out_path: String = read!();
+    
+                println!("");
+    
+                generate_blang_json(path, internal_path, out_path);
+            }
+            2 => {
+                print!("Input the path to the blang's .json file: ");
+                io::stdout().flush().unwrap();
+                let path: String = read!();
+    
+                print!("Input the path to the output file: ");
+                io::stdout().flush().unwrap();
+                let out_path: String = read!();
+    
+                println!("");
+    
+                generate_strings_json(path, out_path);
+            }
+            3 => {
+                print!("Input the path to the mod's .json file: ");
+                io::stdout().flush().unwrap();
+                let path: String = read!();
+    
+                print!("Input the name of the string you want to add: ");
+                io::stdout().flush().unwrap();
+                let identifier: String = read!();
+    
+                println!("");
+    
+                add_new_string(path, identifier);
+            }
+            4 => {
+                print!("Input the path to the vanilla .blang file: ");
+                io::stdout().flush().unwrap();
+                let vanilla_path: String = read!();
+    
+                print!("Input the path to the modified .blang file: ");
+                io::stdout().flush().unwrap();
+                let modified_path: String = read!();
+    
+                print!("Input the language name: ");
+                io::stdout().flush().unwrap();
+                let language: String = read!();
+                let mut internal_path = "strings/".to_string();
+                internal_path.push_str(language.as_str());
+                internal_path.push_str(".blang");
+    
+                print!("Input the path to the output file: ");
+                io::stdout().flush().unwrap();
+                let out_path: String = read!();
+    
+                println!("");
+    
+                modified_blang_to_json(modified_path, vanilla_path, internal_path, out_path);
+            }
+            _ => {
+                exit(101);
+            }
         }
-        2 => {
-            print!("Input the path to the blang's .json file: ");
-            io::stdout().flush().unwrap();
-            let path: String = read!();
 
-            print!("Input the path to the output file: ");
-            io::stdout().flush().unwrap();
-            let out_path: String = read!();
-
-            println!("");
-
-            generate_strings_json(path, out_path);
-        }
-        3 => {
-            print!("Input the path to the mod's .json file: ");
-            io::stdout().flush().unwrap();
-            let path: String = read!();
-
-            print!("Input the name of the string you want to add: ");
-            io::stdout().flush().unwrap();
-            let identifier: String = read!();
-
-            println!("");
-
-            add_new_string(path, identifier);
-        }
-        4 => {
-            print!("Input the path to the vanilla .blang file: ");
-            io::stdout().flush().unwrap();
-            let vanilla_path: String = read!();
-
-            print!("Input the path to the modified .blang file: ");
-            io::stdout().flush().unwrap();
-            let modified_path: String = read!();
-
-            print!("Input the language name: ");
-            io::stdout().flush().unwrap();
-            let language: String = read!();
-            let mut internal_path = "strings/".to_string();
-            internal_path.push_str(language.as_str());
-            internal_path.push_str(".blang");
-
-            print!("Input the path to the output file: ");
-            io::stdout().flush().unwrap();
-            let out_path: String = read!();
-
-            println!("");
-
-            modified_blang_to_json(modified_path, vanilla_path, internal_path, out_path);
-        }
-        _ => {
-            exit(101);
-        }
+        println!("\nPress any key to continue...");
+        let mut stdin = io::stdin();
+        let _ = stdin.read(&mut [0u8]).unwrap();
     }
 }
